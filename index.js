a9f3b914aaf3d5694596794f417d7a6ba0f4610e
Jonathan Rodriguez <jsteven.261190@gmail.com>
2024-04-01 14:43:17 -0500

    5Commit

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(express.json());
 
 // Configuración de CORS
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://bookingcloneapp.netlify.app', 
   credentials: true 
 }));
 

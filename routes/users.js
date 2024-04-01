b8b423852d62d2112c1ecca1e9d5fb92d68c7e31
Jonathan Rodriguez <jsteven.261190@gmail.com>
2024-04-01 15:42:45 -0500

    6Commit

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,34 @@
-// users.js
-
 import express from 'express';
-import { updateUser, deleteUser, getUser, getUsers } from '../controllers/user.controllers.js';
-import { verifyToken, verifyAdmin } from '../utils/verifyToken.js';
+import { deleteUser, getUser, getUsers, updateUser } from '../controllers/user.controllers.js';
+import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
-router.put("/:id", verifyToken, updateUser);
-router.delete("/:id", verifyToken, verifyAdmin, deleteUser); // Aplica verifyAdmin solo para esta ruta
-router.get("/:id", verifyToken, getUser);
-router.get("/", verifyToken, verifyAdmin, getUsers); // Aplica verifyAdmin solo para esta ruta
+/* router.get("/prueba", verifyToken, (req, res, next) => {
+  res.send("Hola, estás autenticado")
+});
+
+router.get("/prueba/:id", verifyUser, (req, res, next) => {
+  res.send("Hola, estás autenticado y autorizado para ver este contenido")
+});
+
+router.get("/admin/:id", verifyAdmin, (req, res, next) => {
+  res.send("Hola Admin, estaás autenticado y puedes hacer todas las acciones")
+}); */
+
+
+//UPDATE
+router.put("/:id", verifyUser, updateUser);
+
+//DELETE
+router.delete("/:id", verifyUser, deleteUser);
+
+//GET
+router.get("/:id", verifyUser, getUser);
+
+//GETING ALL UserS
+router.get("/", verifyAdmin, getUsers);
+
 
 export default router;
 
